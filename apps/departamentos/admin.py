4fb28307075b3198f97516429b7f0b244320bc73
andreFSilva <andrealekhine@msn.com>
2022-02-16 10:13:48 -0300

    adicionando o app departamento

--- apps/departamentos/admin.py
+++ apps/departamentos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Departamento
 
-# Register your models here.
+
+@admin.register(Departamento)
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=150, help_text='Nome do departamento')
+
+    class Meta:
+        verbose_name = 'Departamento'
+        verbose_name_plural = 'Departamentos'
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
